c993afb7f30a5b696a4359ed1275004119a6e33d
zhangzhen1897 <614564049@qq.com>
2020-04-18 00:47:41 +0800

    初始化项目，并在admin后台管理中安装了element-ui

---
+++ admin/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 引入自定义组件和ui库
+import '../src/public/js/common_components.js'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
